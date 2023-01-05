47b09982bac322a97a63d2de51d0b240d013bada
Edwin Pinchao <miguepinchao1992@gmail.com>
2023-01-05 07:45:39 -0600

    Hystrix

--- hystrix-dashboard/src/main/java/com/ks/hystrixdashboard/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/ks/hystrixdashboard/HystrixDashboardApplication.java
@@ -2,10 +2,12 @@ package com.ks.hystrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableHystrix
 public class HystrixDashboardApplication {
 
 	public static void main(String[] args) {
