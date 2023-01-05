49bde5bc3db4a6347991cd2aa0fa792f22e9def1
Edwin Pinchao <miguepinchao1992@gmail.com>
2023-01-05 09:05:35 -0600

    "Config server ,dos microservicios"

--- order-service/src/main/java/com/ks/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/ks/orderservice/service/OrderService.java
@@ -6,17 +6,25 @@ import com.ks.orderservice.common.TransactionResponse;
 import com.ks.orderservice.entity.Order;
 import com.ks.orderservice.reepository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RefreshScope
 public class OrderService {
 
     @Autowired
     private OrderRepository orderRepository;
 
     @Autowired
+    @Lazy
     private RestTemplate template;
+
+   @Value("${microservice.payment-service.endpoints.endpoint.uri}")
+    private String ENDPOINT_URL;
 
     public TransactionResponse saveOrder(TransactionRequest request){
 
@@ -26,7 +34,7 @@ public class OrderService {
         payment.setOrderId(order.getId());
         payment.setAmount(order.getPrice());
         //rest call
-         Payment paymentResponse =template.postForObject( "http://PAYMENT-SERVICE/payment/doPayment",payment, Payment.class);
+         Payment paymentResponse =template.postForObject( ENDPOINT_URL,payment, Payment.class);
       response= paymentResponse.getPaymentStatus().equals("success")?"payment processing succesfulk and order placed":"there is a failure in payment ap, order added to cart";
 
          orderRepository.save(order);
